98a7fad0a62f44a867c57c9b83a0827b88ffdcba
sososoosoo <8216kimsc@gmail.com>
2025-11-11 19:47:07 +0900

    Remove emoji from header logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ function Header() {
     return (
         <header style={styles.header}>
             <Link to="/" style={styles.logo}>
-                LIVELY / 살뜰 🏡
+                LIVELY / 살뜰
             </Link>
             <nav>
                 <Link to="/booking" style={styles.navLink}>예약</Link>
